2e28c7d6d2f751240abd260c679bba6f5d100bed
HHHH <49576153+chinajb2000731@users.noreply.github.com>
2023-01-01 16:35:19 +0800

    Test Reuslt
    Model Code Test Report

---
+++ Test result/Polyspace测试/Module_1/BF_Result_1/.settings/launchingCommand.bat
@@ -0,0 +1,2 @@
+@echo off
+"E:\matlab2019b\polyspace\bin\polyspace-bug-finder.exe" -options-file "E:\matlab\Module_1\BF_Result_1\.settings\options_command.txt" -results-dir "E:\matlab\Module_1\BF_Result_1" %*
---
+++ Test result/Polyspace测试/Module_2/CP_Result_1/.settings/launchingCommand.bat
@@ -0,0 +1,2 @@
+@echo off
+"E:\matlab2019b\polyspace\bin\polyspace-code-prover.exe" -options-file "E:\matlab\Module_2\CP_Result_1\.settings\options_command.txt" -results-dir "E:\matlab\Module_2\CP_Result_1" %*
